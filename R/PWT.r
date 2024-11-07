258aba5897dfdf59b28428a57a70da87790ee35c
HistoricalFool <tatem8499@gmail.com>
2024-11-07 17:10:07 -0500

    gkal

---
+++ R/PWT.r
@@ -0,0 +1,12 @@
+rm(list=ls())
+library(haven)
+library(tidyverse)
+library(ggplot2)
+library(car)
+
+df <- read_dta("pwt1001.dta")
+
+primary_countries <- subset(df, countrycode==c("ITA", "USA", "JPN", "GBR", "CAN", "FRA","SPN"))
+
+alpha <- 1/3
+primary_countries$y <- primary_countries$(rgdpo/pop)
