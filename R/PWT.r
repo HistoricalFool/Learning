201432c50ed087556df14058213044c9aa2760c7
HistoricalFool <tatem8499@gmail.com>
2024-12-16 11:16:57 -0500

    ASdf

--- R/PWT.r
+++ R/PWT.r
@@ -9,4 +9,4 @@ df <- read_dta("pwt1001.dta")
 primary_countries <- subset(df, countrycode==c("ITA", "USA", "JPN", "GBR", "CAN", "FRA","SPN"))
 
 alpha <- 1/3
-primary_countries$y <- primary_countries$(rgdpo/pop)
+primary_countries$y <- primary_countries$
